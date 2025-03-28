83d96d44d1fd3c6a2aefb529e03345eecab47a7a
RaulIQ <raulalimbekov@gmail.com>
2025-03-28 17:37:37 +0300

    add an example where the engine speed is controlled via UART

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,149 @@
 #![no_std]
 #![no_main]
 #![feature(riscv_ext_intrinsics)]
+#![feature(core_intrinsics)]
 
+use core::{arch::{asm, global_asm}, panic::PanicInfo, ptr};
+use mik32v2_pac::{spi_0::delay, Peripherals};
+use riscv::{self as _};
 
-use core::{arch::{global_asm, riscv32::nop}, panic::PanicInfo};
-use mik32v2_pac::{pm, Peripherals};
-
-
-#[unsafe(link_section = ".ram_text")]
-#[inline(never)]
-#[unsafe(no_mangle)]
-pub extern "C" fn plus_one(ptr: *mut i32) {
-    unsafe { ptr.write_volatile(ptr.read_volatile() + 1) };
+fn scale(percent: u32) -> u32 {
+    assert!(percent <= 100);
+    return 40 * percent + 4000
 }
 
-static mut B: u8 = 0;
-static mut T: u8 = 129;
+fn timer_init(p: &Peripherals) {
+    
+    p.pm.clk_apb_p_set().modify({|_, w| w
+        .timer32_1().enable()
+    });
+
+    p.pad_config.pad0_cfg().modify(|_, w| w.port_0_0().func3_interface_or_timer());
+    p.timer32_1.ch1_cntr().modify(|_, w| w
+        .mode().pwm()
+        .en().set_bit()
+    );
+    p.timer32_1.top().modify(|_, w| unsafe { w.tim_top().bits(8000) });
+    p.timer32_1.enable().modify(|_, w| w.tim_clr().set_bit());
+    p.timer32_1.enable().modify(|_, w| w.tim_en().enable());
+}
+
+fn uart_init(p: &Peripherals) {
+    p.pm.clk_apb_p_set().modify({|_, w| w
+        .uart_0().enable()
+    });
+
+    p.pad_config.pad0_cfg().modify(|_, w| w.port_0_6().func2_interface());
+    p.pad_config.pad0_cfg().modify(|_, w| w.port_0_5().func2_interface());
+    p.usart_0.divider().modify(|_, w| unsafe { w.brr().bits(0xd05) });
+    p.usart_0.control1().modify(|_, w| w
+        .te().enable()
+        .re().enable()
+        .ue().enable()
+    );
+
+    while p.usart_0.flags().read().teack().bit_is_clear() {};
+}
+
+fn uart_send_bytes(p: &Peripherals, bytes: &[u8]) {
+    for &byte in bytes {
+        p.usart_0.txdata().modify(|_, w| unsafe { w.tdr().bits(byte.into()) });
+        while p.usart_0.flags().read().tc().bit_is_clear() {};
+    }
+    p.usart_0.txdata().modify(|_, w| unsafe { w.tdr().bits(b'\n'.into()) });
+    while p.usart_0.flags().read().tc().bit_is_clear() {};
+}
+
+fn uart_recieve_bytes(p: &Peripherals, buf: &mut [u8]) -> usize {
+    let mut i = 0;
+
+    loop {
+        while p.usart_0.flags().read().rxne().bit_is_clear() {}
+        let byte = p.usart_0.rxdata().read().rdr().bits() as u8;
+
+        if byte == b'\n' {
+            break;
+        }
+
+        if i < buf.len() {
+            buf[i] = byte;
+            i += 1;
+        }
+    }
+
+    i
+}
+
+fn bytes_to_number(arr: &[u8]) -> u32 {
+    let mut result = 0;
+    for &digit in arr {
+        if digit < b'0' || digit > b'9' { 
+            continue; // Пропускаем некорректные символы
+        }
+        result = result * 10 + (digit - b'0') as u32;
+    }
+    result
+}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn __start_rust() -> ! {
 
-    let p = unsafe { Peripherals::steal() };
-    
-    p.pm.clk_apb_p_set().modify({|_, w| w
-        .uart_0().enable()
-        .gpio_0().enable()
-        .gpio_1().enable()
-        .gpio_2().enable()
-    });
+    let p = Peripherals::take().unwrap();
 
-    p.pm.clk_apb_m_set().modify(|_, w| w
-        .pad_config().enable()
-        .wu().enable()
-        .pm().enable()
-    );
+    timer_init(&p);
+    uart_init(&p);
 
-    p.pad_config.pad2_cfg().modify(|_, w| w.port_2_7().func1_gpio());
-    p.gpio8_2.direction_out().modify(|_, w| unsafe { w.bits(1 << 7) });
+    let bytes = "start".as_bytes();
+    uart_send_bytes(&p, bytes);
+
+    let mut pulse_width = scale(0);
+    p.timer32_1.ch1_ocr().modify(|_, w| unsafe { w.ocr().bits(pulse_width) }); 
+
+    let mut buf = [0; 10];
 
     loop {
-        p.gpio8_2.output().write(|w| unsafe { w.bits(1 << 7) });
-        for _ in 0..4_000_000 { nop() }
-        p.gpio8_2.output().reset();
-        for _ in 0..4_000_000 { nop() }
+        let len = uart_recieve_bytes(&p, &mut buf); 
+        uart_send_bytes(&p, &buf[..len]);
+        let num = bytes_to_number(&buf[..len]);
+
+        let delay = 1000;
+
+        let new_pulse_width = scale(num);
+        if new_pulse_width > pulse_width {
+            while pulse_width < new_pulse_width {
+                pulse_width += 10;
+                p.timer32_1.ch1_ocr().modify(|_, w| unsafe { w.ocr().bits(pulse_width) }); 
+                riscv::asm::delay(delay);
+            }
+            riscv::asm::delay(delay);
+            pulse_width = new_pulse_width;
+            p.timer32_1.ch1_ocr().modify(|_, w| unsafe { w.ocr().bits(pulse_width) }); 
+        } else {
+            while pulse_width > new_pulse_width {
+                pulse_width -= 10;
+                p.timer32_1.ch1_ocr().modify(|_, w| unsafe { w.ocr().bits(pulse_width) }); 
+                riscv::asm::delay(delay);
+            }
+            riscv::asm::delay(delay);
+            pulse_width = new_pulse_width;
+            p.timer32_1.ch1_ocr().modify(|_, w| unsafe { w.ocr().bits(pulse_width) }); 
+        }
     }
 }
 
+
+#[unsafe(export_name = "trap_handler")]
+fn trap() {
+    loop {
+        
+    }
+}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-#[unsafe(export_name = "trap_handler")]
-fn trap() {
-    loop {
-        nop();
-    }
-}
 
 
 global_asm!(
@@ -164,7 +250,7 @@ global_asm!(
             lw      x\index, 4*index(sp)
         .endr
 
-        addi    sp, sp, 128
+        addi sp, sp, 128
         mret
 
     "#
